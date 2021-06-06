d775e80fd7adf48cbd899935c359b20adf048773
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-06 14:30:32 +0530

    Added:part time employee wage

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -2,13 +2,15 @@ package com.bridgelabz;
 
 public class EmpWageComputation {
 	static final int Is_Full_Time = 1;
+	static final int Is_Part_Time = 2;
 	static int empRatePerHr = 20;
 	static int empHrs = 0;
 	static int empWage = 0;
 
 	public static void main(String[] args) {
 		EmpWageComputation emp = new EmpWageComputation();
-		isEmployeePresent(emp);
+		addPartTime(emp);
+		calculateDailyWage(emp);
 	}
 
 	/**
@@ -37,6 +39,18 @@ public class EmpWageComputation {
 		if (empCheck == Is_Full_Time)
 			empHrs = 8;
 		empWage = empHrs * empRatePerHr;
-		System.out.println("daily wage of an employee" + empWage);
+		System.out.println("daily wage of an employee " + empWage);
+	}
+
+	/**
+	 * This method is to add part time employee and calculating wage
+	 * 
+	 * @param emp
+	 */
+	private static void addPartTime(EmpWageComputation emp) {
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Is_Part_Time)
+			empHrs = 4;
+		empWage = empHrs * empRatePerHr;
 	}
 }
