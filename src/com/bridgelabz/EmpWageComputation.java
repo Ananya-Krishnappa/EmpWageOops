9a1fd465b26ec9159a51e6ca40472c3fead9bd1d
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-06 14:09:23 +0530

    Added:check if employee is present

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
 
 public class EmpWageComputation {
+	static final int Is_Full_Time = 1;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to emp wage computation");
+		EmpWageComputation emp = new EmpWageComputation();
+		isEmployeePresent(emp);
+	}
+
+	/**
+	 * This method is checking if employee is present or not
+	 * 
+	 * @param emp
+	 * @return
+	 */
+	private static boolean isEmployeePresent(EmpWageComputation emp) {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Is_Full_Time) {
+			System.out.println("employee is present");
+		} else
+			System.out.println("employee is not present");
+		return false;
 	}
 }
